349677cd2a47eee65a20b26736291480e894fdb7
wuxinxin2020 <2252018672@qq.com>
2022-08-04 17:08:16 +0800

    thread pool

--- src/com/jingming/juc/Pool.java
+++ src/com/jingming/juc/Pool.java
@@ -2,26 +2,31 @@ package com.jingming.juc;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Pool {
 	
 	public static void main(String[] args) throws InterruptedException {
 		int nThreads = 2;
-		Executor executor = Executors.newFixedThreadPool(nThreads);
+		ExecutorService executor = Executors.newFixedThreadPool(nThreads);
 		Set<Thread> set = new HashSet<>();
 		final ThreadLocal<String> th = new ThreadLocal<String>();
-		for (int i = 0; i < 1000000; i++) {
+		for (int i = 0; i < 100; i++) {
 			final int index = i;
 			executor.execute(()->{
 				th.set("i=" + index);
-				set.add(Thread.currentThread());
-//				th.remove();
+				Thread currentThread = Thread.currentThread();
+				set.add(currentThread);
+				//String s = th.get();
+				//th.remove();
+				//System.out.println(s);
 			});
 		}
 		Thread.sleep(1000);
 		System.out.println(set.size());
+		System.out.println(th.get());
+		executor.shutdown();
 	}
 
 }
